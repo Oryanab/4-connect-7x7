e2e38ebcb0748475a70225291bc290b2c44fe1e6
Oryanab <86202122+Oryanab@users.noreply.github.com>
2021-10-30 13:43:59 +0300

    update 2.0 - initialize variables

--- index.js
+++ index.js
@@ -1,11 +1,25 @@
 "use strict";
+const allArrays = require("./possibiloties");
 
+// regular variables
 class Model {
-  constructor() {}
+  constructor() {
+    this.currentPlayer = 1;
+    this.winneingArrays = allArrays.winneingArray;
+  }
 }
 
+// build html structure
 class View {
-  constructor() {}
+  constructor() {
+    const parentGrid = this.getElement(".grid");
+    const gameDiv = createElement("div");
+    const borderDivs = createElement("div", "taken");
+    const squares = this.getElementAll(".grid div");
+    const result = this.getElement("#results");
+    const resetBtn = this.getElement("#reset");
+    const displayCurrentPlayer = this.getElement("#current-player");
+  }
 
   createElement(tag, className) {
     const element = document.createElement(tag);
@@ -17,12 +31,29 @@ class View {
     const element = document.querySelector(selector);
     return element;
   }
+
+  getElementAll(selector) {
+    const element = document.querySelectorAll(selector);
+    return element;
+  }
+
+  render(gameDiv , borderDivs) {
+    for (let createDiv = 0; createDiv < 48; createDiv++) {
+      parentGrid.appendChild(gameDiv);
+    }
+    for (let blockDiv = 0; blockDiv < 8; blockDiv++) {
+      parentGrid.appendChild(borderDivs);
+    }
+  }
 }
 
+// append all functionality
 class Controller {
   #model;
   #view;
-  constructor(model, view) {}
+  constructor(model, view) {} 
+  render()
+
 }
 
 const app = new Controller(new Model(), new View());
